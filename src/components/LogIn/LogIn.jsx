dc34d07344ddd33cc2585e2dffb79f71f37d41b9
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-07-23 23:43:20 +0300

    adding html markup for login page

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -3,7 +3,11 @@ import './LogIn.css';
 function LogIn() {
   return (
     <div className="logIn">
-      logIn
+        <h1>Log In</h1>
+        <input type="text" placeholder='Name'/>
+        <input type="text" placeholder='Surname'/>
+        <input type="password" placeholder='Password'/>
+        <button>login</button>
     </div>
   );
 }
