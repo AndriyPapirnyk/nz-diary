2777ac2327effa47247e1b422c81c72396302f51
OstapoKapo <OstapoKapo@gmail.com>
2023-07-23 23:24:11 +0300

    welcome page

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,7 +3,10 @@ import './Welcome.css';
 function Welcome() {
   return (
     <div className="welcome">
-      welcome
+      <div className="welcome__row">
+        <div className="welcome__btn">Login</div>
+        <div className="welcome__btn">SignUp</div>
+      </div>
     </div>
   );
 }
