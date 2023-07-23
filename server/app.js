3ec7f243ff7694db5b98325cc6887bb927a2440d
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-07-23 23:09:52 +0300

    clearing files of node server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const Student = require('./models/student-model');
 const DBroutes = require('./routes/route');
 require('dotenv').config();
-const ejs = require('ejs');
-const PORT = 3000;
+
+const PORT = 8000;
 const url = `mongodb+srv://andriy:andriy12345@nz.beqns0u.mongodb.net/?retryWrites=true&w=majority`;
 const app = express();
 const bodyParser = require('body-parser')
@@ -28,29 +27,29 @@ async function connect() {
 connect();
 
 
-app.get('/signup', (req,res) => {
-    res.sendFile(__dirname + '/signUP.html')
-})
+// app.get('/signup', (req,res) => {
+//     res.sendFile(__dirname + '/signUP.html')
+// })
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/startPage.html')
-})
+// app.get('/', (req,res) => {
+//     res.sendFile(__dirname + '/startPage.html')
+// })
 
-app.get('/login', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+// app.get('/login', (req,res) => {
+//     res.sendFile(__dirname + '/index.html')
+// })
 
-app.get('/teacher', (req,res) => {
-    res.sendFile(__dirname + '/teacher.html')
-})
+// app.get('/teacher', (req,res) => {
+//     res.sendFile(__dirname + '/teacher.html')
+// })
 
-app.get('/start', (req,res) => {
-    res.sendFile(__dirname + '/startPage.html')
-})
+// app.get('/start', (req,res) => {
+//     res.sendFile(__dirname + '/startPage.html')
+// })
 
-app.get('/main', (req,res) => {
-    res.sendFile(__dirname + '/main.html')
-})
+// app.get('/main', (req,res) => {
+//     res.sendFile(__dirname + '/main.html')
+// })
 
 app.listen(PORT, () => {
     console.log(`Server works on port: ${PORT}`)
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,21 +1,9 @@
 const express = require('express');
 const Student = require('../models/student-model');
 
-const {
-    getStudent,
-    signUp,
-    logIn
-} = require('../controllers/controller')
+
 
 const router = express.Router();
-
-router.get('/students', getStudent);
-
-router.post('/signUp', signUp);
-
-router.post('/login', logIn);
-
-
 
 
 module.exports = router;
