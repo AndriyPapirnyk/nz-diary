437c4fb31af6b05e624fa3414a7050e5cfe1ecf4
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-07-23 23:50:46 +0300

    adding axious

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,10 +1,11 @@
 import './LogIn.css';
+import axios from 'axios';
 
 function LogIn() {
   return (
     <div className="logIn">
         <h1>Log In</h1>
-        <input type="text" placeholder='Name'/>
+        <input  type="text" placeholder='Name'/>
         <input type="text" placeholder='Surname'/>
         <input type="password" placeholder='Password'/>
         <button>login</button>
