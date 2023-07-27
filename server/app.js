df530b289388d15f233f22dff015250d3a96144a
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-07-27 22:34:37 +0300

    log in beta ver

--- server/app.js
+++ server/app.js
@@ -32,6 +32,33 @@ app.get('/', cors(), (req, res) => {
     res.send('test')
 })
 
+app.post('/login', async(req, res) => {
+
+    const inputData = req.body.inputValues;
+
+    const data = {
+        email: inputData.logIn__inputEmail,
+        password: inputData.logIn__inputPassword,
+    }
+
+    console.log(data)
+
+    try{
+
+        const verify = await Student.findOne({email: data.email, password: data.password});
+
+        if(verify) {
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(404);         
+        }        
+    }
+    catch(error) {
+        console.log(error)
+    }
+
+});
+
 app.post('/signup', async(req, res) => {
 
     const inputData = req.body.inputValues;
--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,5 +1,7 @@
-import './LogIn.css';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import './LogIn.css';
 
 //
 
@@ -7,17 +9,58 @@ import star1 from './img/star1.png';
 import star2 from './img/star2.png';
 
 function LogIn() {
+
+
+  const navigate = useNavigate();
+
+    const [inputValues, setInputValues] = useState({
+        logIn__inputEmail: '',
+        logIn__inputPassword: '',
+    });
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInputValues((prevValues) => ({
+          ...prevValues,
+          [name]: value,
+        }));
+    };
+
+
+
+    async function sendSignUpData(e) {
+
+      e.preventDefault();
+
+      try {
+          await axios.post('http://localhost:8000/login', {inputValues})
+          .then((response) => {
+              response.status === 200 ? navigate('/home') : alert('somethin went wrong');
+          })
+          .catch((error) => {
+              console.error('Error:', error);
+          });
+      }
+      catch(error) {
+          console.log(error);
+      }
+    };
+
+
+
+
+
   return (
     <div className="logIn">
       <div className="logIn__container">
-        <form action="submit">
+        <form action="POST">
             <img className='logIn__star' src={star1} alt="" />
             <img className='logIn__star' src={star2} alt="" />
           <h1>Log In</h1>
-          <input  type="text" placeholder='Name'/>
-          <input type="text" placeholder='Surname'/>
-          <input type="password" placeholder='Password'/>
-          <button type='submit'>Log In</button>
+          <input value={inputValues.logIn__inputEmail} onChange={handleChange} type="email" className="logIn__input" placeholder='Email' name="logIn__inputEmail"/>
+          <input value={inputValues.logIn__inputPassword} onChange={handleChange} type="password" className="logIn__input" placeholder='Pasword' name="logIn__inputPassword"/>
+          <button type='submit' onClick={sendSignUpData} >Log In</button>
         </form>
       </div>
     </div>
